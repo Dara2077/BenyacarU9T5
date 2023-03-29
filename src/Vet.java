7b99063ec2698d8aeadd1dd11e3f4e19634f63d4
dara2077 <dbenyacar1@gmail.com>
2023-03-29 09:30:54 -0400

    Work from class

--- src/Vet.java
+++ src/Vet.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Vet {
+import java.util.ArrayList;
+
+public class Vet {
+    private String name;
+    private ArrayList<Animal> clients;
+
+    public Vet (String name){
+        this.name = name;
+        clients = new ArrayList<Animal>();
+    }
+
+    public void addClient(Animal animal){
+        String currName = animal.getName();
+        boolean isClientAlready = false;
+        if (clients.contains(animal)){
+            isClientAlready = true;
+        }
+        if (isClientAlready){
+            System.out.println(currName + " is already a client of " + name);
+        }
+        else {
+            clients.add(animal);
+            System.out.println("Welcome to " + name + "'s office, " + currName);
+        }
+    }
 }
